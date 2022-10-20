b1ea57cc92e25068e5b8304becb5e861310d4881
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2022-10-19 20:21:05 -0700

    set up logger

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import config from 'config';
 import express from 'express';
-
+import logger from './utils/logger';
 const port = config.get<number>('port');
 const app = express();
 
@@ -11,5 +11,5 @@ app.use(express.json());
 const AWS_ACCESS_KEY_ID = config.get<string>('accessKeyId');
 console.log(AWS_ACCESS_KEY_ID);
 app.listen(port, async () => {
-  console.log(`port${port}`);
+  logger.info(`App is running on port ${port} `);
 });
